39c016f72d89c4c81a88043db6902822bc128516
Rubendvb <rubenvasquez556@gmail.com>
2025-01-23 14:37:38 -0300

    moving images

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,172 @@ import './App.css'
 function App() {
   return (
     <>
-      Desserts Waffle with Berries Waffle 6.50 Add to Cart Vanilla Bean Crème
-      Brûlée Crème Brûlée 7.00 Add to Cart Macaron Mix of Five Macaron 8.00 Add
-      to Cart Classic Tiramisu Tiramisu 5.50 Add to Cart Pistachio Baklava
-      Baklava 4.00 Add to Cart Lemon Meringue Pie Pie 5.00 Add to Cart Red
-      Velvet Cake Cake 4.50 Add to Cart Salted Caramel Brownie Brownie 4.50 Add
-      to Cart Vanilla Panna Cotta Panna Cotta 6.50 Add to Cart Your Cart{' '}
-      {/* Quantity -->) */}
-      Your added items will appear here Challenge by{' '}
-      <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
-      Coded by <a href="#">Your Name Here</a>.
+      <div>
+        <main>
+          <h1>Desserts</h1>
+
+          <div>
+            <article>
+              <div>
+                <img src="" alt="" />
+                <button>Add to Cart</button>
+              </div>
+              <div>
+                <span>Waffle</span>
+                <h3>Waffle with Berries</h3>
+                <span>6.50</span>
+              </div>
+            </article>
+
+            <article>
+              <div>
+                <img src="" alt="" />
+                <button>Add to Cart</button>
+              </div>
+              <div>
+                <span>Crème Brûlée</span>
+                <h3>Vanilla Bean Crème Brûlée</h3>
+                <span>7.00</span>
+              </div>
+            </article>
+
+            <article>
+              <div>
+                <img src="" alt="" />
+                <button>Add to Cart</button>
+              </div>
+              <div>
+                <span>Macaron</span>
+                <h3>Macaron Mix of Five</h3>
+                <span>8.00</span>
+              </div>
+            </article>
+
+            <article>
+              <div>
+                <img src="" alt="" />
+                <button>Add to Cart</button>
+              </div>
+              <div>
+                <span>Tiramisu</span>
+                <h3>Classic Tiramisu</h3>
+                <span>5.50</span>
+              </div>
+            </article>
+
+            <article>
+              <div>
+                <img src="" alt="" />
+                <button>Add to Cart</button>
+              </div>
+              <div>
+                <span>Baklava</span>
+                <h3>Pistachio Baklava</h3>
+                <span>4.00</span>
+              </div>
+            </article>
+
+            <article>
+              Lemon
+              <div>
+                <img src="" alt="" />
+                <button>Add to Cart</button>
+              </div>
+              <div>
+                <span>Pie</span>
+                <h3>Meringue Pie</h3>
+                <span>5.00</span>
+              </div>
+            </article>
+
+            <article>
+              Red
+              <div>
+                <img src="" alt="" />
+                <button>Add to Cart</button>
+              </div>
+              <div>
+                <span>Cake</span>
+                <h3>Velvet Cake</h3>
+                <span>4.50</span>
+              </div>
+            </article>
+
+            <article>
+              <div>
+                <img src="" alt="" />
+                <button>Add to Cart</button>
+              </div>
+              <div>
+                <span>Brownie</span>
+                <h3>Salted Caramel Brownie</h3>
+                <span>4.50</span>
+              </div>
+            </article>
+
+            <article>
+              <div>
+                <img src="" alt="" />
+                <button>Add to Cart</button>
+              </div>
+              <div>
+                <span>Panna Cotta</span>
+                <h3>Vanilla Panna Cotta</h3>
+                <span>6.50</span>
+              </div>
+            </article>
+          </div>
+        </main>
+
+        <aside>
+          <h2>Your Cart {/* Quantity -->) */}</h2>
+
+          <div>
+            <img src="" alt="" />
+            <p>Your added items will appear here</p>
+          </div>
+
+          <div>
+            <ul>
+              <li>
+                <div>
+                  <span>Classic</span>
+                  <div>
+                    <span>1x</span>
+                    <span>@ $5.50</span>
+                    <span>$5.50</span>
+                  </div>
+                </div>
+                <div>
+                  <img src="" alt="" />
+                </div>
+              </li>
+
+              <li>
+                <div>
+                  <span>Vanilla</span>
+                  <div>
+                    <span>4x</span>
+                    <span>@ $7.00</span>
+                    <span>$28.00</span>
+                  </div>
+                </div>
+                <div>
+                  <img src="" alt="Vanilla Item" />
+                </div>
+              </li>
+            </ul>
+          </div>
+        </aside>
+      </div>
+
+      <footer>
+        Challenge by{' '}
+        <a href="https://www.frontendmentor.io?ref=challenge">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </footer>
     </>
   )
 }
