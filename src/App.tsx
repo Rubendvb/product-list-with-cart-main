c2175ef21b689a3ef5d42960d0ba7c965d701477
Rubendvb <rubenvasquez556@gmail.com>
2025-02-01 16:44:53 -0300

    stiling itens

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.scss'
 import { ButtonAddToCart } from './components/ButtonAddToCart'
 
 function App() {
-  const [items, setItems] = useState(1)
+  const [items, setItems] = useState(2)
 
   return (
     <>
@@ -17,7 +17,7 @@ function App() {
               <div className="dessert__image-container">
                 <img
                   className="dessert__image"
-                  src="/public/assets/images/image-waffle-desktop.jpg"
+                  src="/assets/images/image-waffle-desktop.jpg"
                   alt="Waffle"
                 />
 
@@ -37,7 +37,7 @@ function App() {
               <div className="dessert__image-container">
                 <img
                   className="dessert__image"
-                  src="/public/assets/images/image-creme-brulee-desktop.jpg"
+                  src="/assets/images/image-creme-brulee-desktop.jpg"
                   alt="Crème Brûlée"
                 />
                 <div className="container__button">
@@ -55,7 +55,7 @@ function App() {
               <div className="dessert__image-container">
                 <img
                   className="dessert__image"
-                  src="/public/assets/images/image-creme-brulee-desktop.jpg"
+                  src="/assets/images/image-creme-brulee-desktop.jpg"
                   alt="Macaron"
                 />
 
@@ -74,7 +74,7 @@ function App() {
               <div className="dessert__image-container">
                 <img
                   className="dessert__image"
-                  src="/public/assets/images/image-creme-brulee-desktop.jpg"
+                  src="/assets/images/image-creme-brulee-desktop.jpg"
                   alt="Tiramisu"
                 />
 
@@ -93,7 +93,7 @@ function App() {
               <div className="dessert__image-container">
                 <img
                   className="dessert__image"
-                  src="/public/assets/images/image-creme-brulee-desktop.jpg"
+                  src="/assets/images/image-creme-brulee-desktop.jpg"
                   alt="Baklava"
                 />
 
@@ -113,7 +113,7 @@ function App() {
               <div className="dessert__image-container">
                 <img
                   className="dessert__image"
-                  src="/public/assets/images/image-creme-brulee-desktop.jpg"
+                  src="/assets/images/image-creme-brulee-desktop.jpg"
                   alt="Lemon Pie"
                 />
 
@@ -133,7 +133,7 @@ function App() {
               <div className="dessert__image-container">
                 <img
                   className="dessert__image"
-                  src="/public/assets/images/image-creme-brulee-desktop.jpg"
+                  src="/assets/images/image-creme-brulee-desktop.jpg"
                   alt="Red Velvet Cake"
                 />
 
@@ -152,7 +152,7 @@ function App() {
               <div className="dessert__image-container">
                 <img
                   className="dessert__image"
-                  src="/public/assets/images/image-creme-brulee-desktop.jpg"
+                  src="/assets/images/image-creme-brulee-desktop.jpg"
                   alt="Brownie"
                 />
 
@@ -171,7 +171,7 @@ function App() {
               <div className="dessert__image-container">
                 <img
                   className="dessert__image"
-                  src="/public/assets/images/image-creme-brulee-desktop.jpg"
+                  src="/assets/images/image-creme-brulee-desktop.jpg"
                   alt="Panna Cotta"
                 />
 
@@ -195,7 +195,7 @@ function App() {
             <div className="cart__empty-message">
               <img
                 className="cart__empty-image"
-                src="/public/assets/images/illustration-empty-cart.svg"
+                src="/assets/images/illustration-empty-cart.svg"
                 alt="Empty Cart"
               />
               <p className="cart__empty-text">
@@ -207,44 +207,90 @@ function App() {
               <ul className="cart__list">
                 <li className="cart__item">
                   <div className="cart__item-details">
-                    <span className="cart__item-type">Classic</span>
+                    <span className="cart__item-type">Classic Tiramisu</span>
                     <div className="cart__item-info">
                       <span className="cart__item-quantity">1x</span>
                       <span className="cart__item-unit-price">@ $5.50</span>
                       <span className="cart__item-total-price">$5.50</span>
                     </div>
                   </div>
-                  <div className="cart__item-image">
-                    <img
-                      className="cart__item-image-element"
-                      src=""
-                      alt="Classic Item"
-                    />
-                  </div>
+                  <img
+                    className="cart__item-close"
+                    src="/assets/images/icon-remove-item.svg"
+                    alt="icon remove item"
+                  />
                 </li>
 
                 <li className="cart__item">
                   <div className="cart__item-details">
-                    <span className="cart__item-type">Vanilla</span>
+                    <span className="cart__item-type">
+                      Vanilla Bean Crème Brûlée
+                    </span>
                     <div className="cart__item-info">
                       <span className="cart__item-quantity">4x</span>
                       <span className="cart__item-unit-price">@ $7.00</span>
                       <span className="cart__item-total-price">$28.00</span>
                     </div>
                   </div>
+                  <img
+                    className="cart__item-close"
+                    src="/assets/images/icon-remove-item.svg"
+                    alt="icon remove item"
+                  />
+                </li>
+
+                <li className="cart__item">
+                  <div className="cart__item-details">
+                    <span className="cart__item-type">Vanilla Panna Cotta</span>
+                    <div className="cart__item-info">
+                      <span className="cart__item-quantity">2x</span>
+                      <span className="cart__item-unit-price">@ $6.50</span>
+                      <span className="cart__item-total-price">$13.00</span>
+                    </div>
+                  </div>
+                  <img
+                    className="cart__item-close"
+                    src="/assets/images/icon-remove-item.svg"
+                    alt="icon remove item"
+                  />
                 </li>
               </ul>
             </div>
           )}
+
+          <div className="cart__order-total">
+            <span className="cart__order-total-label">Order Total</span>
+            <span className="cart__order-total-amount">$46.50</span>
+          </div>
+
+          <div className="cart__carbon-neutral">
+            <img
+              className="cart__carbon-neutral-image"
+              src="/assets/images/icon-carbon-neutral.svg"
+              alt=""
+            />
+            <p className="cart__carbon-neutral-text">
+              This is a <span>carbon-neutral</span> delivery
+            </p>
+          </div>
+
+          <button className="cart__confirm-button">Confirm Order</button>
         </aside>
       </div>
 
-      <footer>
+      <footer className="footer">
         Challenge by{' '}
-        <a href="https://www.frontendmentor.io?ref=challenge">
+        <a
+          className="footer__link"
+          href="https://www.frontendmentor.io?ref=challenge"
+        >
           Frontend Mentor
         </a>
-        . Coded by <a href="#">Your Name Here</a>.
+        . Coded by{' '}
+        <a className="footer__link" href="#">
+          Rubén
+        </a>
+        .
       </footer>
     </>
   )
